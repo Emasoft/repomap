2c4f25275b4388235998d307a514bb36dd21086a
Emanuele Sabetta <fmuaddib@gmail.com>
2025-03-07 07:49:56 +0100

    ok

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RepoMap',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='emanuelesabetta',
+    author_email='',
+    description=''
+)
